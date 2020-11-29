0292d3e008f060e9e03f7566e1c5ada44268ad25
cristianluca19 <cristianlucatti@gmail.com>
2020-11-29 16:46:13 -0300

    cambios card

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,7 @@ function App() {
   function onSearch(ciudad) {
     //Llamado a la API del clima
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=4ae2636d8dfbdc3044bede63951a019b`)
-      .then(r => r.json())
+      .then(r =>{ return  r.json()})
       .then((recurso) => {
         if(recurso.main !== undefined){
           const ciudad = {
